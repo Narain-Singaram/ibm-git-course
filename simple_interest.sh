27920a9af1228e1d49adde1f69d4b00de4673ac6
Narain Singaram <112223598+Narain-Singaram@users.noreply.github.com>
2023-08-12 20:19:28 -0400

    Create simple_interest.sh

---
+++ simple_interest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Simple Interest Calculator
+
+# Function to calculate simple interest
+calculate_simple_interest() {
+  principal=$1
+  rate=$2
+  time=$3
+  interest=$(echo "scale=2; $principal * $rate * $time / 100" | bc)
+  echo "Principal Amount: $principal"
+  echo "Interest Rate: $rate%"
+  echo "Time (years): $time"
+  echo "Simple Interest: $interest"
+}
+
+# Read input values
+read -p "Enter Principal Amount: " principal
+read -p "Enter Interest Rate (percent): " rate
+read -p "Enter Time (in years): " time
+
+# Call the function to calculate simple interest
+calculate_simple_interest "$principal" "$rate" "$time"
